e725f194a6c42fb5942b2f9fe74cf313fe2a178f
unknown <lakku@naver.com>
2022-02-04 13:28:43 +0900

    new file

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.all('/secret', function (req, res, next) {
+    console.log('Accessing the secret section ...');
+    next(); // pass control to the next handler
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Server is working : PORT - ', port);
+});
+
